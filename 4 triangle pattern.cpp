b5bcd0b9f6d64aa63a75aefb3a2887787f4909b6
Shaswat SInha <97851942+Lone0023@users.noreply.github.com>
2022-01-18 15:42:28 +0530

    Update 4 triangle pattern.cpp

--- 4 triangle pattern.cpp
+++ 4 triangle pattern.cpp
@@ -33,3 +33,32 @@ int main()
     }
     return 0;
 }
+
+
+
+//Alternate way to print a triangle with star could be to add a space between every star.
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, i , j;
+    cout<<"Enter the value of n: "<<endl;
+    cin>>n;
+    cout<<endl;
+    cout<<"Your Triangle is printed below."<<endl<<endl;
+
+    for(i=1; i<=n; i++){
+        for(j=1; j<=n; j++){
+            if(j<=(n-i)){
+                cout<<" ";
+            }
+        }
+                for(j=1; j<=i; j++){
+                    cout<<"*"<<" ";
+                    }
+        cout<<endl;
+    }
+    return 0;
+}
+*/
