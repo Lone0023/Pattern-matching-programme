e082ce864e3443816f2bcca8ac475a7f6f7cb81b
Shaswat SInha <97851942+Lone0023@users.noreply.github.com>
2022-01-19 09:41:08 +0530

    Add files via upload

---
+++ 5.1 Hollow rhombus pattern.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int n, i , j;
+    cout<<"Enter the value of n: "<<endl;
+    cin>>n;
+    cout<<endl;
+    cout<<"Your Rhombus is printed below."<<endl<<endl;
+
+    for(i = 1; i<=n; i++){
+        for(j = 1; j<=(n-i); j++){
+            cout<<" ";
+        }
+            for(j=1; j<=n; j++){
+                    if(i==1 || i==n){
+                        cout<<" * ";
+                    }
+                    else if(j==1 || j==n){
+                        cout<<" * ";
+                    }
+                    else{
+                        cout<<"   ";
+                    }
+            }
+       cout<<endl;
+    }
+    return 0;
+
+}
